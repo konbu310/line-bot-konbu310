228f2a6f8441734edda4934fcd426e918e8b9446
yuya <yuyas203@gmail.com>
2018-02-20 23:31:08 +0900

    環境変数の追加とポイント追加の実装

--- server.js
+++ server.js
@@ -4,19 +4,17 @@ const puppeteer = require('puppeteer')
 const PORT = process.env.PORT || 3000
 
 const config = {
-  channelAccessToken: 'S6jOttKiR5kfz6HxunhVm41qErLs/IsKDbbO2/lJ7zRyepQgcMDQOJyb6Mb4jE4MGguTDyFDAFu4rQ4BHnb/f+JEwByqh/gZqu9sOo2KUyfNyjkAX0V/OKmvoUvp1U1r6YcV5Si2ump4X8VpEksfzAdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'e9af9dcb1d398998f9ffefc889fc4686'
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
 }
 
 const app = express()
 
 app.post('/talk', line.middleware(config), (req, res) => {
-  // req.body.events should be an array of events
   if (!Array.isArray(req.body.events)) {
     return res.status(500).end()
   }
 
-  // handle events separately
   Promise.all(req.body.events.map(handleEvent))
     .then(() => res.end())
     .catch((err) => {
@@ -43,20 +41,19 @@ const handleEvent = (event) => {
   if (/^[d,D][n ]?([0-9]*)$/.test(event.message.text)) {
     let code = RegExp.$1
     if (code.length !== 12) { return replyText(event.replyToken, 'コードは12桁で入力してください。') }
-    replyText(event.replyToken, code)
+    addPoint(event.replyToken, code)
   }
 }
 
-const addPoint = async (code, token) => {
-  if (code.length !== 12) { return client.replyMessage(token, 'コードは12桁で入力してください。') }
+const addPoint = async (token, code) => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto(`https://www.dan-on.com/jp-ja/my-danpoints?code=${code}`, {waitUntil: 'domcontentloaded'})
-  await page.type('#signin-email', 'serendip001@icloud.com')
-  await page.type('#signin-password', 'dp4530')
+  await page.type('#signin-email', process.env.USER_NAME)
+  await page.type('#signin-password', process.env.PASSWORD)
   await page.click('div.box__footer >  div.check-form > button[type="submit"]')
   await page.waitFor(8000)
-  const result = await page.content().then(content => {
+  const resultMessage = await page.content().then(content => {
     let successFlag = /<header class="popin__header">/
     let pointMatch = /<strong>(\d*)<\/strong> ポイント<\/span>/
     if (successFlag.test(content)) {
@@ -65,13 +62,8 @@ const addPoint = async (code, token) => {
     return 'ポイントが間違っているか、使用済みの可能性があります。'
   })
   await browser.close()
-  return client.replyMessage(token, result)
+  replyText(token, resultMessage)
 }
 
 app.listen(PORT)
 console.log(`Server running at ${PORT}`)
-
-/*
-/^[d,D]n[ ]?([0-9]*)$/.test(event.message.text))
-addPoint(RegExp.$1, event.replyToken)
-*/
