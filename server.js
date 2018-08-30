9ba4fa525e54770f161983d2d5a8023c460ee2b8
yuya <yuyas203@gmail.com>
2018-08-30 21:41:10 +0900

    gaeように調整

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
 const puppeteer = require('puppeteer');
+
 const PORT = process.env.PORT || 3000;
+const SUCCESS_CHECK = /<header class="popin__header">/;
+const POINT_CHECK = /<strong>(\d*)<\/strong> ポイント<\/span>/;
+
+let RESULT_MESSAGE;
 
 const config = {
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -9,6 +14,10 @@ const config = {
 };
 
 const app = express();
+
+app.get('/', (req, res) => {
+	res.send('問題なく動いています');
+});
 
 app.post('/talk', line.middleware(config), (req, res) => {
 	if (!Array.isArray(req.body.events)) {
@@ -38,8 +47,9 @@ const handleEvent = event => {
 	}
 
 	let userMessage = event.message.text;
-	if (/^[d,D][n ]?([0-9]*)$/.test(userMessage)) {
+	if (/^(?:d|D|ダノン){1}([0-9]+)$/.test(userMessage)) {
 		let code = RegExp.$1;
+		console.log(code);
 		if (code.length !== 12) {
 			return replyText(event.replyToken, 'コードは12桁で入力してください。');
 		}
@@ -51,32 +61,39 @@ const handleEvent = event => {
 
 const addPoint = async (token, code) => {
 	const browser = await puppeteer.launch({
-		args: ['--no-sandbox', '--disable-setuid-sandbox']
+		args: ['--no-sandbox'],
+		ignoreHTTPSErrors: true
 	});
 	const page = await browser.newPage();
-	await page.goto(`https://www.dan-on.com/jp-ja/my-danpoints?code=${code}`, {
-		waitUntil: 'domcontentloaded'
-	});
+
 	try {
+		await page.goto(`https://www.dan-on.com/jp-ja/my-danpoints?code=${code}`, {
+			waitUntil: 'domcontentloaded'
+		});
+
+		await page.waitFor(2 * 1000);
+
 		await page.type('#signin-email', process.env.DANON_USER_NAME);
 		await page.type('#signin-password', process.env.DANON_PASSWORD);
+
 		await page.click(
 			'div.box__footer >  div.check-form > button[type="submit"]'
 		);
+
+		await page.waitFor(10 * 1000);
+
+		await page.content().then(content => {
+			RESULT_MESSAGE = SUCCESS_CHECK.test(content)
+				? `現在のポイントは ${content.match(POINT_CHECK)[1]} です。`
+				: 'コードが間違っているか、使用済みの可能性があります。';
+		});
 	} catch (err) {
-		return replyText(token, err.name + ': ' + err.message);
+		console.log(err);
+		RESULT_MESSAGE = 'エラーが発生しました。';
 	}
-	await page.waitFor(8000);
-	const resultMessage = await page.content().then(content => {
-		let successFlag = /<header class="popin__header">/;
-		let pointMatch = /<strong>(\d*)<\/strong> ポイント<\/span>/;
-		if (successFlag.test(content)) {
-			return `現在のポイントは ${content.match(pointMatch)[1]} です。`;
-		}
-		return 'ポイントが間違っているか、使用済みの可能性があります。';
-	});
+
 	await browser.close();
-	return replyText(token, resultMessage);
+	return replyText(token, RESULT_MESSAGE);
 };
 
 app.listen(PORT);
