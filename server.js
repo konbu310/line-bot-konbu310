909c039c66b0270bb00da5e98088921b92ead050
Yuya Sato <yuyas203@gmail.com>
2018-09-27 23:08:30 +0900

    返信の仕方を変えた

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
-const puppeteer = require('puppeteer');
-
-const PORT = process.env.PORT || 3000;
-const SUCCESS_CHECK = /<header class="popin__header">/;
-const POINT_CHECK = /<strong>(\d*)<\/strong> ポイント<\/span>/;
-
-let RESULT_MESSAGE;
+const express = require("express");
+const line = require("@line/bot-sdk");
+const puppeteer = require("puppeteer");
 
 const config = {
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 	channelSecret: process.env.CHANNEL_SECRET
 };
 
+const client = new line.Client(config);
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('問題なく動いています');
+let RESULT_MESSAGE;
+let RESULT_POINT;
+const FAILURE_CHECK = /<p class="dn-icon-delete">/;
+const SUCCESS_CHECK = /<p class="dn-icon-check">/;
+const POINT_CHECK = /<strong>(\d*)<\/strong> ポイント<\/span>/;
+
+app.get("/", (req, res) => {
+	return res.send("問題なく稼働中です！");
 });
 
-app.post('/talk', line.middleware(config), (req, res) => {
+app.post("/talk", line.middleware(config), (req, res) => {
 	if (!Array.isArray(req.body.events)) {
 		return res.status(500).end();
 	}
@@ -32,69 +34,80 @@ app.post('/talk', line.middleware(config), (req, res) => {
 		});
 });
 
-const client = new line.Client(config);
-
-const replyText = (token, message) => {
-	return client.replyMessage(token, {
-		type: 'text',
+const pushMessage = async message => {
+	await client.pushMessage(process.env.GROUP_ID, {
+		type: "text",
 		text: message
 	});
 };
 
-const handleEvent = event => {
-	if (event.type !== 'message' || event.message.type !== 'text') {
+const handleEvent = async event => {
+	if (event.type !== "message" || event.message.type !== "text") {
 		return Promise.resolve(null);
 	}
 
-	let userMessage = event.message.text;
-	if (/^(?:d|D|ダノン){1}([0-9]+)$/.test(userMessage)) {
-		let code = RegExp.$1;
-		console.log(code);
-		if (code.length !== 12) {
-			return replyText(event.replyToken, 'コードは12桁で入力してください。');
+	if (/^(?:d|D|ダノン){1}([0-9]+)$/.test(event.message.text)) {
+		if (RegExp.$1.length !== 12) {
+			await pushMessage("コードは12桁だよ！\n間違ってないか確認して！");
+			return Promise.resolve(null);
 		}
-		return addPoint(event.replyToken, code);
+		await addPoint(RegExp.$1);
 	} else {
 		return Promise.resolve(null);
 	}
+
+	return Promise.resolve(null);
 };
 
-const addPoint = async (token, code) => {
+const addPoint = async code => {
+	await pushMessage("ポイント追加するね！");
+
 	const browser = await puppeteer.launch({
-		args: ['--no-sandbox'],
+		args: ["--no-sandbox"],
 		ignoreHTTPSErrors: true
 	});
+
 	const page = await browser.newPage();
 
 	try {
 		await page.goto(`https://www.dan-on.com/jp-ja/my-danpoints?code=${code}`, {
-			waitUntil: 'domcontentloaded'
+			waitUntil: "domcontentloaded"
 		});
 
 		await page.waitFor(2 * 1000);
 
-		await page.type('#signin-email', process.env.DANON_USER_NAME);
-		await page.type('#signin-password', process.env.DANON_PASSWORD);
+		await page.type("#signin-email", process.env.DANON_USER_NAME);
+		await page.type("#signin-password", process.env.DANON_PASSWORD);
+
+		await pushMessage("ポチポチ...\nもうちょっと待ってねφ(..)");
 
 		await page.click(
-			'div.box__footer >  div.check-form > button[type="submit"]'
+			"#connect_form > div.box__footer.pushlog-form_cta > div > button"
 		);
 
 		await page.waitFor(10 * 1000);
 
 		await page.content().then(content => {
-			RESULT_MESSAGE = SUCCESS_CHECK.test(content)
-				? `現在のポイントは ${content.match(POINT_CHECK)[1]} です。`
-				: 'コードが間違っているか、使用済みの可能性があります。';
+			if (FAILURE_CHECK.test(content)) {
+				RESULT_POINT = content.match(POINT_CHECK)[1];
+				RESULT_MESSAGE = `コードが間違ってない？\nもしかしたら使用済みかも(;_:)`;
+			} else if (SUCCESS_CHECK.test(content)) {
+				RESULT_POINT = content.match(POINT_CHECK)[1];
+				RESULT_MESSAGE = `ポイントを追加したよ！\n今のポイントは、${RESULT_POINT}だよ(´ε｀ )`;
+			}
 		});
 	} catch (err) {
 		console.log(err);
-		RESULT_MESSAGE = 'エラーが発生しました。';
+		RESULT_MESSAGE = "問題が起きてるみたい(´・ω・`)";
 	}
 
+	await pushMessage(RESULT_MESSAGE);
+
 	await browser.close();
-	return replyText(token, RESULT_MESSAGE);
+
+	return Promise.resolve(null);
 };
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
